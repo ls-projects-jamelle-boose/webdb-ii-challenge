9da3155f66f66930fe46fa9b03c86287ae1a29c1
Jamelle-Boose <19831942+Jamelle-Boose@users.noreply.github.com>
2020-04-12 04:45:59 -0700

    feat: add `delete` operations

--- api/routes/cars.js
+++ api/routes/cars.js
@@ -1,11 +1,11 @@
-const express = require("express"),
-  cars = require("../controllers/cars"),
+const express = require('express'),
+  cars = require('../controllers/cars'),
   router = express.Router();
 
-router.get("/", cars.all);
-router.post("/", cars.new);
-router.get("/:id", cars.one);
-// router.delete("/:id", cars.rm);
+router.get('/', cars.all);
+router.post('/', cars.new);
+router.get('/:id', cars.one);
+router.delete('/:id', cars.rm);
 // router.put("/:id", cars.update);
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,18 +1,19 @@
-const express = require("express"),
-  morgan = require("morgan"),
-  helmet = require("helmet"),
-  carRouter = require("./routes"),
+require('pretty-error').start();
+const express = require('express'),
+  morgan = require('morgan'),
+  helmet = require('helmet'),
+  carRouter = require('./routes'),
   server = express();
 
 server.use(express.json());
 server.use(helmet());
-server.use(morgan("dev"));
-server.use("/api", carRouter);
+server.use(morgan('dev'));
+server.use('/api', carRouter);
 
-server.get("/", (req, res) => {
+server.get('/', (req, res) => {
   let root = {
     message: `Working on root.`,
-    status: 200
+    status: 200,
   };
 
   res.status(200).json(root);
